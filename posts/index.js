2138d341adfd7ce3ff74e7dd47d36653a1e2bf21
Sunny Dubey <sunnydubey2502@gmail.com>
2023-10-11 18:44:52 +0530

    event bus

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { randomBytes } = require('crypto');
+const axios = require('axios');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
